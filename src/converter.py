791b2baa980250dc5c049b6ef633875c4d35b124
clmntgbr <clement.goubier@gmail.com>
2025-02-19 23:22:13 +0100

    Add preset subtitle options and update MediaPod structure in Protobuf definitions

--- src/converter.py
+++ src/converter.py
@@ -1,5 +1,5 @@
 import json
-from src.Protobuf.Message_pb2 import ApiToSubtitleMerger, MediaPod, Video
+from src.Protobuf.Message_pb2 import ApiToSubtitleMerger, MediaPod, Video, Preset
 
 class ProtobufConverter:
     @staticmethod
@@ -13,14 +13,33 @@ class ProtobufConverter:
         video.size = int(media_pod_data["originalVideo"]["size"])
         video.audios.extend(media_pod_data["originalVideo"]["audios"])
         video.subtitles.extend(media_pod_data["originalVideo"]["subtitles"])
+
+        video.IsInitialized()
+
+        preset = Preset()
+        preset.subtitleFont = media_pod_data["preset"]["subtitleFont"]
+        preset.subtitleSize = media_pod_data["preset"]["subtitleSize"]
+        preset.subtitleColor = media_pod_data["preset"]["subtitleColor"]
+        preset.subtitleBackground = media_pod_data["preset"]["subtitleBackground"]
+        preset.subtitleOutlineColor = media_pod_data["preset"]["subtitleOutlineColor"]
+        preset.subtitleOutlineThickness = media_pod_data["preset"]["subtitleOutlineThickness"]
+        preset.subtitleShadow = media_pod_data["preset"]["subtitleShadow"]
+        preset.subtitleShadowColor = media_pod_data["preset"]["subtitleShadowColor"]
+
+        preset.IsInitialized()
         
         media_pod = MediaPod()
         media_pod.uuid = media_pod_data["uuid"]
         media_pod.userUuid = media_pod_data["userUuid"]
         media_pod.status = media_pod_data["status"]
         media_pod.originalVideo.CopyFrom(video)
+        media_pod.preset.CopyFrom(preset)
+
+        media_pod.IsInitialized()
 
         proto_message = ApiToSubtitleMerger()
         proto_message.mediaPod.CopyFrom(media_pod)
+
+        proto_message.IsInitialized()
 
         return proto_message
